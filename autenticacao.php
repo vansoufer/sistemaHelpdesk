06e45b4effb4fb823b2e5b2c669d7acbf0754c06
Vanessa Souto <vansouferr@gmail.com>
2024-07-24 17:37:54 -0300

    feat: new layout forgot password

--- autenticacao.php
+++ autenticacao.php
@@ -1,24 +1,34 @@
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8" />
- 	<title>Sistema de HelpDesk</title>
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<link rel="stylesheet" type="text/css" href="css/estilo.css" >
+	<title>ARV-DESK</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 	<link rel="stylesheet" type="text/css" href="css/autenticacao.css">
-
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
-<div class="loginbox">
-	<img src="imagens/user.png" class="avatar">
-	<script  type="text/javascript">
-		function loginsucessfully(){ //função que controla o tempo de espera de redirecionamento
-			setTimeout("window.location = 'painel.php'", 4000);
-		}
+	<div class="flex-column">
+		<div class="left">
+			<img src="imagens/img-index.png" class="">
+			<div class="text-container">
+				<h1>Bem vindo(a) !</h1>
+				<p>Informe o login e senha para acessar o sistema.</p>
+			</div>
+		
+		</div>
+		<div class="loginbox">
+			
+			<h1>ARV-DESK</h1>
+			
+		<script  type="text/javascript">
+			function loginsucessfully(){ //função que controla o tempo de espera de redirecionamento
+				setTimeout("window.location = 'painel.php'", 4000);
+			}
 
-		function loginfailed(){
-			setTimeout("window.location = 'index.html'", 4000);
-		}
-	</script>
+			function loginfailed(){
+				setTimeout("window.location = 'index.html'", 4000);
+			}
+		</script>
 
 
 <?php
@@ -36,36 +46,44 @@ $senhaSegura = addslashes($senha);
 
 $sql = mysqli_query($conexao,"SELECT * FROM usuarios WHERE login = '$login' LIMIT 1") or die (mysqli_error());
 $userData = mysqli_fetch_assoc($sql);
-
-if(password_verify($senha, $userData['senha'])){
-	$nivel = $userData['nivel'];
-	$nome = $userData['nome'];
-
-	if($nivel == 'adm') {
-		$_SESSION["login"]=$_POST["login"];
-		$_SESSION["senha"]=$_POST["senha"];
-		$_SESSION['adm'] = $nome;
+if($userData){
+	if(password_verify($senha, $userData['senha'])){
+		$nivel = $userData['nivel'];
+		$nome = $userData['nome'];
+	
+		if($nivel == 'adm') {
+			$_SESSION["login"]=$_POST["login"];
+			$_SESSION["senha"]=$_POST["senha"];
+			$_SESSION['adm'] = $nome;
+		}
+		else if ($nivel == 'tec') {
+			$_SESSION["login"]=$_POST["login"];
+			$_SESSION["senha"]=$_POST["senha"];
+			$_SESSION['tec'] = $nome;
+		}
+	
+		else if ($nivel == 'comum') {
+			$_SESSION["login"]=$_POST["login"];
+			$_SESSION["senha"]=$_POST["senha"];
+			$_SESSION['comum'] = $nome;
+		}
+	
+		echo "<br><center class='success'>Efetuando Login! Aguarde um instante...</center>";
+		echo ' <br><center><div class="loader"></div></center>';
+		echo "<script>loginsucessfully()</script>";
+	}else{
+		echo "<br><br><center><span class='foco-erro'>Dados inválidos!</span><br><span class='feedback-await'>Aguarde um instante e tente novamente.</span></center>";
+		echo ' <br><center><div class="loader"></div></center>';
+		echo "<script>loginfailed()</script>";
 	}
-	else if ($nivel == 'tec') {
-		$_SESSION["login"]=$_POST["login"];
-		$_SESSION["senha"]=$_POST["senha"];
-		$_SESSION['tec'] = $nome;
-	}
-
-	else if ($nivel == 'comum') {
-		$_SESSION["login"]=$_POST["login"];
-		$_SESSION["senha"]=$_POST["senha"];
-		$_SESSION['comum'] = $nome;
-	}
-
-	echo "<br><br><center><span class='foco-sucesso'>Efetuando Login!</span><br><br> Aguarde um instante.</center>";
-	echo "<script>loginsucessfully()</script>";
+	
 }else{
-	echo "<br><br><center><span class='foco-erro'>Dados inválidos!</span><br><br>Aguarde um instante e tente novamente.</center>";
+	echo "<br><br><center><span class='foco-erro'>Dados inválidos!</span><br><span class='feedback-await'>Aguarde um instante e tente novamente.</span></center>";
+	echo ' <br><center><div class="loader"></div></center>';
 	echo "<script>loginfailed()</script>";
 }
 
-echo ' <br><center><div class="loader"></div></center>';
+
 ?>
 
 </div>
--- cad-chamado-senha.php
+++ cad-chamado-senha.php
@@ -5,26 +5,33 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="../css/formstyle.css">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link rel="stylesheet" type="text/css" href="css/autenticacao.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<title>ARVDESK</title>
 </head>
-<body>
-
+<body class="body-forgot-password">
+<div class="back forgot-password">
+<div class="content">
+	<form class="form-forgot-password">
+	<h1>ARV-DESK</h1>
 <?php
 	
 	include ("conexao.php");
 	
 
 
-	$tipo = $_POST['tipo'];
-	$dtAbertura = $_POST['dtAbertura'];
+	$tipo = 2;
+	$dtAbertura = date('Y-m-d');
 	
 	$NomeRequerente = $_POST['NomeRequerente'];
 	$setor = $_POST['setor'];
-	$tpReparo = $_POST['tpReparo'];
+	$tpReparo = 5;
 	
-	$status = $_POST['status'];
-	$prioridade = $_POST['prioridade'];
-	$atribuicao = $_POST['atribuicao'];
+	$status = 1;
+	$prioridade = 1;
+	$atribuicao = 1;
 	$descricao = $_POST['descricao'];
 	
 
@@ -33,23 +40,35 @@
 		(tipo, dtAbertura, NomeRequerente,  setor, tpReparo, status, prioridade, atribuicao, descricao) VALUES 
 		('$tipo', '$dtAbertura', '$NomeRequerente', '$setor', '$tpReparo', '$status', '$prioridade','$atribuicao', '$descricao')");
 	
-	echo "
-	
-	<form>
-	<div class='grupo' align='center'>
-	<img src='./imagens/sucesso.png' alt='some text' width=80 height=80 align='center'><br><br>
-	</div>
-	<div>
-	<font color=#333><center><h1>Solicitação enviada com sucesso !</h1></center>
-	</div>
-	</form>
-	";
+	if($sql){
+		echo "
+		
+		<div class='grupo' align='center'>
+		<i class='bi bi-check-circle-fill'></i><br><br>
+		</div>
+		<div>
+		<font color=#333><center><h1>Solicitação enviada com sucesso !</h1></center>
+		</div>
+		";
+	}else if($sqp->error){
+		echo "
+		
+		<div class='grupo' align='center'>
+		<i class='bi bi-exclamation-triangle-fill'></i><br><br>
+		</div>
+		<div>
+		<font color=#333><center><h1>Ocorreu um erro ao enviar solicitação, entre em contato com o Administrador.</h1></center>
+		</div>
+		";
+	}
 	mysqli_close($conexao);
 	
 	
  ?>
 
- 	<center><a href="index.html">Clique aqui para retornar a pagina de login</a></center>
-
+ 	<center><a href="index.html" class="go-back">Clique aqui para retornar a pagina de login</a></center>
+</form>
+</div>
+</div>
 </body>
 </html>
